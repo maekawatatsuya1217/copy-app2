79b64438643bc90873ccdee6c4f62c08ae1ddace
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-02 10:29:12 +0900

    usersコントローラーを編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    before_action :set_user, only: :show
+
+    def show
+        @nickname = @user.nickname
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
 end
