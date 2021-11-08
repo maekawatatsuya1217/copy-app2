926ae6edcf5d3e67513e72a2081709bd23ed11fc
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-08 11:18:06 +0900

    コントローラーを編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,10 @@
 class TasksController < ApplicationController
+
+  before_action :authenticate_user!, only: [:index]
+  before_action :tasks_definition, only: [:index]
+
   def index
+    @status = ['todo', 'advice', 'memo']  
   end
 
   def show
@@ -12,5 +17,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def tasks_definition
+    @tasks = Task.where(user_id: current_user.id).includes(:user).order('created_at DESC')
   end
 end
