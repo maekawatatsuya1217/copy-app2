f20371055a880a5d047ccc3298b3dc420773ce38
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-08 10:59:38 +0900

    ルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/store'
-  get 'tasks/update'
-  get 'tasks/destroy'
+  get    'tasks'     => 'tasks#index'
+  post   'tasks'     => 'tasks#store'
+  get    'tasks/:id' => 'tasks#show'
+  put    'tasks/:id' => 'tasks#update'
+  delete 'tasks/:id' => 'tasks#destroy'
   devise_for :users
   root to: "tops#index"
   resources :users, only: [:show, :edit, :update]
