d86681d209a00de79317fe474bd799d28f511de2
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-08 10:57:09 +0900

    tasksコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/store'
+  get 'tasks/update'
+  get 'tasks/destroy'
   devise_for :users
   root to: "tops#index"
   resources :users, only: [:show, :edit, :update]
