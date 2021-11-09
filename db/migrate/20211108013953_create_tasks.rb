8cb24fea1d455435efafe3ac19a70946067c4b31
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-09 13:52:04 +0900

    テーブルの修正

--- db/migrate/20211108013953_create_tasks.rb
+++ db/migrate/20211108013953_create_tasks.rb
@@ -1,9 +1,17 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.text :state
-      t.text :task
-      t.date :limit_date
+      # タイトル
+      t.string :title, null: false
+      # 説明
+      t.text :description, null: true
+      # 締め切り
+      t.datetime :target_at, null: true
+      # タスク完了日
+      t.datetime :completed_at, null: true
+      # タスク完了したか
+      t.boolean :completed, null: false, default: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
