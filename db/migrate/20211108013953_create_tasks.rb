767d768c850f409bf95c66c21337fa77ffa51ac2
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-11 13:23:24 +0900

    テーブルを編集

--- db/migrate/20211108013953_create_tasks.rb
+++ db/migrate/20211108013953_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.string :title, null: false
+      t.string :title,     null: false
       t.text :description, null: true
-      t.references :user, foreign_key: true
+      t.references :user,  foreign_key: true
       t.timestamps
     end
   end
