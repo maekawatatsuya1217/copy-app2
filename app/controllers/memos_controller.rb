d90d82221fa4ae6c8ad4a9ea437ae2cea21d2bf1
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-11-11 14:01:18 +0900

    new,createアクションを作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -6,5 +6,22 @@ class MemosController < ApplicationController
         @memos = current_user.memos
     end
 
-    
+    def new
+        @memo = Memo.new
+    end
+
+    def create
+        @memo = current_user.memos.new(memo_params)
+        if @memo.save
+            redirect_to tasks_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def memo_params
+        params.require(:memo).permit(:title, :description)
+    end
 end
